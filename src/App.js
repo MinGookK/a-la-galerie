14b4fee04856b3b97f94712f743e428298a53756
MinGook <mgk8609@naver.com>
2021-06-03 00:09:06 +0900

    .

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+import { PhotoContextProviderWrapper } from "./context/PhotoContext";
+import { ClockContextProviderWrapper } from "./context/ClockContext";
+
+import Nav from "./component/nav/Nav";
+import Clock from "./component/clock/Clock";
+import Gallery from "./container/gallery/Gallery";
+import RedCarpet from "./component/gallery-image/RedCarpet";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Nav />
+      <Clock />
+      <PhotoContextProviderWrapper>
+        <Gallery />
+      </PhotoContextProviderWrapper>
+      <RedCarpet />
+    </div>
+  );
+};
+
+export default App;
